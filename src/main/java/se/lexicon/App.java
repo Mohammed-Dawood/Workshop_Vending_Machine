8842682b74330ad738be75c17af9017899e6a8ed
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-07-02 12:34:20 +0300

    Step_2 done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,7 +1,25 @@
 package se.lexicon;
 
+import se.lexicon.model.Drink;
+import se.lexicon.model.Snack;
+import se.lexicon.model.Toy;
+
 public class App {
     public static void main(String[] args) {
 
+        Snack snack = new Snack("123-abc", 100 , "Kex" , "Chocolate");
+
+        Drink drink = new Drink("456-def", 200 , "Cola" , 33);
+
+        Toy toy = new Toy("456-def", 300 , "Pistol" , 3);
+
+        System.out.println(snack.examine());
+        System.out.println(snack.use());
+
+        System.out.println(drink.examine());
+        System.out.println(drink.use());
+
+        System.out.println(toy.examine());
+        System.out.println(toy.use());
     }
 }
